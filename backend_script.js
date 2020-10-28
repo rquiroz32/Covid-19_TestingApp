8e5dd1069df4504d1a4c904d680336bc3bec658e
rquiroz3290 <rquiroz3290@gmail.com>
2020-10-27 20:48:44 -0400

    resolved fatal bug that came up for everyone with magic

--- backend_script.js
+++ backend_script.js
@@ -10,23 +10,8 @@ $(document).ready(function () {
     var ratingArray = [];
     var phoneNumber = "";
     var phoneNumberArray = [];
-   
-    var testSiteObject = {
-        address: '',
-        phone: '',
-        website:'',
-        lat:'',
-        long:'',
-        img:''
 
-
-    }
-
-
-  var mapImgApiKey = "Z7n3t2fnai6LHriDt0pVcxWyZec1O8JJROrBAgjJlZM";
-
-    
-
+    var mapImgApiKey = "Z7n3t2fnai6LHriDt0pVcxWyZec1O8JJROrBAgjJlZM";
 
 
     //API key for hereAPI
@@ -34,9 +19,14 @@ $(document).ready(function () {
     var zipLat;
     var zipLong;
 
-    // Returns Geo Positionging of the location, currently hardcoded to 07005
+
 
     var zipCode;
+
+
+
+    ////////////Modal initialization (I'm not sure but it seems this needs to be done before all modals are actually called)
+    //$('.modal').modal();
 
 
     // on click event to search by zip code and return covid test sites
@@ -45,20 +35,41 @@ $(document).ready(function () {
 
         zipCode = $("#search-box").val().trim();
 
-        oldSearchArray.push(zipCode);
+        var  zipCodeNumCheck = parseInt(zipCode)
 
-        localStorage.setItem("past_zipcodes", oldSearchArray);
+        var zipString = zipCode.toString();
 
-        geoPosition_and_TestingSites();
+        //console.warn("length of zipcode entered is " + zipString.length)
 
-        //change location to results page
-        
-        setTimeout(function(){ 
-            
-            window.location.href = "./result.html"; }, 500
+        //if (!zipCode || zipCode === NaN ||zipString.length != 5) {
 
-            );
-        
+
+            //Open the modal for the error message
+            //$('#modal1').modal('open');
+
+            //alert("THIS IS AN ERROR MESSAGE")
+            //return
+
+
+        //else {
+
+            oldSearchArray.push(zipCode);
+
+            localStorage.setItem("past_zipcodes", oldSearchArray);
+
+            geoPosition_and_TestingSites();
+
+            //change location to results page
+
+            // setTimeout(function () {
+
+            //     window.location.href = "./result.html";
+            // }, 500
+
+            // ); // closes set timeout
+
+        //}
+        //}
 
     })//closes zip code search on click event
 
@@ -99,8 +110,8 @@ $(document).ready(function () {
                     phoneNumberArray.push(phoneNumber);
 
                     //In local storage we now have ratings and rakings for the last thing that was searched
-                    localStorage.setItem("lastratingsarray",ratingArray);
-                    localStorage.setItem("lastphonenumber",phoneNumberArray);
+                    localStorage.setItem("lastratingsarray", ratingArray);
+                    localStorage.setItem("lastphonenumber", phoneNumberArray);
 
                 });//Closing fxn after ajax call for google places
 
@@ -153,7 +164,7 @@ $(document).ready(function () {
             zipLat = response.items[0].position.lat
             zipLong = response.items[0].position.lng
             console.log(response)
-            
+
 
 
             // build out query for test site locations
@@ -167,29 +178,28 @@ $(document).ready(function () {
 
                 for (i = 0; i < response.items.length; i++) {
 
-                    
-                        testSiteObject.address = response.items[i].address.label.slice(23),
-                        testSiteObject.phone = response.items[i].contacts[0].phone[0].value,
-                        testSiteObject.website = response.items[i].contacts[0].www[0].value,
-                        testSiteObject.lat = response.items[i].access[0].lat,
-                        testSiteObject.long =  response.items[i].access[0].lng,
-                        testSiteObject.img = "https://image.maps.ls.hereapi.com/mia/1.6/mapview?poi=" + zipLat + "," + zipLong + "&poitxs=16&poitxc=black&poifc=yellow&z=14&apiKey=" + mapImgApiKey
-
-
-
-                    
+                    testSiteObject = {
+                        address: response.items[i].address.label.slice(23),
+                        phone: response.items[i].contacts[0].phone[0].value,
+                        website: response.items[i].contacts[0].www[0].value,
+                        lat: response.items[i].access[0].lat,
+                        long: response.items[i].access[0].lng,
+                        img: "https://image.maps.ls.hereapi.com/mia/1.6/mapview?poi=" + zipLat + "," + zipLong + "&poitxs=16&poitxc=black&poifc=yellow&z=14&apiKey=" + mapImgApiKey
+                        
+                    }
 
                     testSiteArray.push(testSiteObject);
-                  
 
 
-                    
+
                     console.log(response)
                     console.log(testSiteObject.img);
 
 
                 }
                 //Feeding this information into a function to get more information
+                console.log("test site array is " + testSiteArray)
+
                 getInfoFromGoogle(testSiteArray);
 
 
